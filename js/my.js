99e4add02ee45de5697e556f41d0a5ca81e0dcf7
Ahmed <a.alkamel25@gmail.com>
2021-01-06 13:53:10 +0200

    first commit

---
+++ js/my.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+
+    $(".owl-carousel").owlCarousel({
+        items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+    });
+
+    // ----------------------------------
+
+    $(".m-menu span.close").on("click",function(){
+        $(".m-menu").animate({
+            "right":"-303px"
+        },400);
+    });
+
+    $(".nav-menu ul li:first-of-type a").on("click",function(e){
+        e.preventDefault();
+        $(".m-menu").animate({
+            "right":"0px"
+        },400);
+    });
+
+
+});
+
+$(window).on("load",function(){
+    $(".loader-wrapper").delay(2500).fadeOut();
+
+});
